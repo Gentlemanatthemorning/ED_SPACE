68879e9b883e37f8fa1927d04f8a342e937725b2
Sergei Kalchenko <lucifer@macbook-pro-2.home>
2018-11-29 23:40:16 +0100

    Added logo for contsct page. Changed images in memory game. Added background for header

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,14 +1,24 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Contact extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Hello Contact!</h1>
+  render() {
+    return (
+      <section>
+        <div className="main__logo">
+          <div className="container">
+            <div className="parallax__4" />
+            <div className="container__fill__4" />
+          </div>
         </div>
-      )
-    }
+        <footer>
+          <p>&copy; #ED_SPACE 2018</p>
+          <Link to="/contact" className="instagram" />
+          <Link to="/contact" className="facebook" />
+        </footer>
+      </section>
+    );
   }
-  
-  export default Contact;
+}
+
+export default Contact;
